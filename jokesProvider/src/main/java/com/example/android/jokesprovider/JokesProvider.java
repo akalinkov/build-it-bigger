b17f2c41c23759f35aec6fbaf9a34cc25010e40f
Aliaksei Kalinkou <aliaksei.kalinkou@gmail.com>
2018-09-03 20:14:29 -0400

    Button displays a joke from JokesProvider

---
+++ jokesProvider/src/main/java/com/example/android/jokesprovider/JokesProvider.java
@@ -0,0 +1,9 @@
+package com.example.android.jokesprovider;
+
+public class JokesProvider {
+
+    public static String getJoke() {
+        return "Q: How does a tech guy drink coffee?\n" +
+                "A: He installs Java!";
+    }
+}
